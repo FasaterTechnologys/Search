bfe0c10388ccf8d31aa5f992db3cee122b8b5c0a
technologysfasater <69322615+FasaterTechnologys@users.noreply.github.com>
2020-10-30 19:08:32 +0300

    SeaechPro

--- lib/Home/Home.dart
+++ lib/Home/Home.dart
@@ -12,7 +12,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.yellow,
       body: Search(context),
     );
   }
--- lib/Home/inputSearch.dart
+++ lib/Home/inputSearch.dart
@@ -1,48 +1,166 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:searchpro/Home/button.dart';
 
-Widget inputLogin(IconData icon, String hint, TextEditingController controller,
-    bool obsecure, BuildContext context) {
-  bool OnAndOffSearchIcon = false;
-  return Padding(
-    padding: const EdgeInsets.only(bottom: 70),
-    child: Stack(
+class InputLog extends StatefulWidget {
+  InputLog({Key key}) : super(key: key);
+
+  @override
+  InputLogState createState() => InputLogState();
+}
+
+class InputLogState extends State<InputLog> {
+  TextEditingController SarchController = TextEditingController();
+  FocusNode _focus = FocusNode();
+  String proverka;
+  Random random = Random();
+
+  @override
+  void initState() {
+    super.initState();
+    _focus.addListener(_onFocusChange);
+  }
+
+  void ProverkaPading() {
+    if (proverka == "true") {
+      left = 270;
+      height = 0.64;
+      iconColor = Colors.blueAccent;
+      containerColor = Colors.white;
+      setState(() {});
+    } else if (proverka == "false") {
+      left = 145;
+      height = 0.0;
+      iconColor = null;
+      containerColor = null;
+      setState(() {});
+    }
+  }
+
+  void _onFocusChange() {
+    proverka = _focus.hasFocus.toString();
+    ProverkaPading();
+  }
+
+//0.6
+  double left = 145;
+  double height = 0.0;
+  Color iconColor = null;
+  Color containerColor = null;
+  Color containerSearchColor = Colors.black;
+  @override
+  Widget build(BuildContext context) {
+    return inputLogin(
+        Icons.search, "что искать", SarchController, false, context);
+  }
+
+  Widget inputLogin(IconData icon, String hint,
+      TextEditingController controller, bool obsecure, BuildContext context) {
+    controller.addListener(() {
+      setState(() {
+        containerSearchColor = Color.fromRGBO(
+            random.nextInt(255), random.nextInt(255), random.nextInt(255), 1);
+      });
+    });
+    return Stack(
       children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 110, left: 5),
+          child: AnimatedContainer(
+            duration: Duration(
+              seconds: 4,
+            ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Colors.white,
+            ),
+            alignment: Alignment.topCenter,
+            width: MediaQuery.of(context).size.width * 0.975,
+            height: MediaQuery.of(context).size.height * height,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 16),
+              child: GridView.builder(
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: 16,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.only(top: 9, left: 10, right: 10),
+                    child: Container(
+                      color: containerSearchColor,
+                    ),
+                  );
+                },
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 3.8,
+                ),
+              ),
+            ),
+          ),
+        ),
         Column(
           children: [
             Padding(
               padding: const EdgeInsets.only(top: 100),
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height * 0.06,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(60),
-                    border: Border.all(width: 2, color: Colors.grey)),
-                child: TextField(
-                  controller: controller,
-                  obscureText: obsecure,
-                  style: TextStyle(fontSize: 20, color: Colors.black),
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintStyle: TextStyle(
-                      fontSize: 20,
-                      color: Color.fromRGBO(153, 155, 158, 1),
-                    ),
-                    hintText: hint,
-                    //focusedBorder: OutlineInputBorder(
-                    // borderSide: BorderSide(color: Colors.black, width: 2),
-                    //),
-                    //enabledBorder: OutlineInputBorder(
-                    //  borderSide: BorderSide(color: Colors.black, width: 1),
-                    // ),
-                  ),
+              child: Center(
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.98,
+                  height: MediaQuery.of(context).size.height * 0.06,
+                  decoration: BoxDecoration(
+                      color: containerColor,
+                      borderRadius: BorderRadius.circular(60),
+                      border: Border.all(width: 2, color: Colors.grey)),
                 ),
               ),
             ),
-            button(),
           ],
         ),
+        Padding(
+          padding: const EdgeInsets.only(top: 95, left: 15),
+          child: TextField(
+            focusNode: _focus,
+            controller: controller,
+            obscureText: obsecure,
+            style: TextStyle(fontSize: 20, color: Colors.black),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              hintStyle: TextStyle(
+                fontSize: 20,
+                color: Color.fromRGBO(153, 155, 158, 1),
+              ),
+              hintText: hint,
+              //focusedBorder: OutlineInputBorder(
+              //borderSide: BorderSide(color: Colors.black, width: 2),
+              //),
+              //enabledBorder: OutlineInputBorder(
+              //borderSide: BorderSide(color: Colors.black, width: 1),
+              //),
+            ),
+          ),
+        ),
+        AnimatedPadding(
+          duration: Duration(
+            seconds: 4,
+          ),
+          padding: EdgeInsets.only(top: 103, left: left),
+          child: Container(
+            width: MediaQuery.of(context).size.width * 0.23,
+            height: MediaQuery.of(context).size.height * 0.05,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(60),
+              color: iconColor,
+            ),
+            child: Center(
+              child: Icon(
+                Icons.search,
+                size: 35,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
       ],
-    ),
-  );
+    );
+  }
 }
